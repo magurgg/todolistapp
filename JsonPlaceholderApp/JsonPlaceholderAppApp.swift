ccbe5deb59ae6394c4d5706d4bc388dfc16128e1
Marco Gutiérrez <marco.gutierrez@accenture.com>
2023-10-31 10:23:37 -0700

    Initial Commit

---
+++ JsonPlaceholderApp/JsonPlaceholderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPlaceholderAppApp.swift
+//  JsonPlaceholderApp
+//
+//  Created by marco.gutierrez on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct JsonPlaceholderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
